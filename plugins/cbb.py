4a4029d096a86a8147d85fc72489072318a0d508
anubhavgaver-droid <anubhavgaver@gmail.com>
2026-08-20 09:45:48 +0530

    Update admin check for settings button handler

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -57,8 +57,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
 
     # ⚙️ SETTINGS BUTTON HANDLER (ONLY FOR ADMIN)
     elif data == "cb_settings":
-        if query.from_user.id not in OWNER_ID:
-            return await query.answer("⚠️ दिस इज ओनली फॉर एडमिन! (This is only for Admin)", show_alert=True)
+        if query.from_user.id not in ADMINS:
+            return await query.answer("⚠️ This is only for Admin ⚠️", show_alert=True)
         await query.answer()
         await send_main_settings_panel(query)
 
