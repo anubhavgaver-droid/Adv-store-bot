1c29181d75ee8fe7b0fd3651bb816bc3b5665922
anubhavgaver-droid <anubhavgaver@gmail.com>
2026-09-10 08:36:27 +0530

    Update admin filters for bot commands

--- plugins/useless.py
+++ plugins/useless.py
@@ -28,7 +28,7 @@ from database.database import *
 
 #=====================================================================================##
 
-@Bot.on_message(filters.command('stats') & admin)
+@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now(timezone.utc)
     delta = now - bot.uptime
@@ -43,29 +43,17 @@ WAIT_MSG = "<b>Working....</b>"
 #=====================================================================================##
 
 
-@Bot.on_message(filters.command('users') & filters.private & admin)
+@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await db.full_userbase()
     await msg.edit(f"{len(users)} users are using this bot")
 
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 #=====================================================================================##
 
 #AUTO-DELETE
 
-@Bot.on_message(filters.private & filters.command('dlt_time') & admin)
+@Bot.on_message(filters.private & filters.command('dlt_time') & filters.user(ADMINS))
 async def set_delete_time(client: Bot, message: Message):
     try:
         duration = int(message.command[1])
@@ -77,22 +65,10 @@ async def set_delete_time(client: Bot, message: Message):
     except (IndexError, ValueError):
         await message.reply("<b>Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴠᴀʟɪᴅ ᴅᴜʀᴀᴛɪᴏɴ ɪɴ sᴇᴄᴏɴᴅs.</b> Usage: /dlt_time {duration}")
 
-@Bot.on_message(filters.private & filters.command('check_dlt_time') & admin)
+@Bot.on_message(filters.private & filters.command('check_dlt_time') & filters.user(ADMINS))
 async def check_delete_time(client: Bot, message: Message):
     duration = await db.get_del_timer()
 
     await message.reply(f"<b><blockquote>Cᴜʀʀᴇɴᴛ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇʀ ɪs sᴇᴛ ᴛᴏ {duration}sᴇᴄᴏɴᴅs.</blockquote></b>")
 
 #=====================================================================================##
-
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
